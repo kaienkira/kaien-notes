bcbed09785199acb46f8c7bc793ad0ca3c924544
kaien <kaienkira@gmail.com>
2015-10-30 20:46:30 +0800

    work sync

--- bin/run_cppcheck.sh
+++ bin/run_cppcheck.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-cppcheck --enable=all --quiet *.cpp *.cc *.h 2>&1 | \
-grep -v -e 'The function.*is never used' \
-        -e 'Cppcheck cannot find all the include files'
+cppcheck --enable=all --quiet -j4 . 2>&1
