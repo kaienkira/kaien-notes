0f10150b684794c4cc97118f675975a2c0828865
kaien <kaienkira@gmail.com>
2018-08-21 15:01:33 +0800

    work sync

--- util/show_socket_options.cc
+++ util/show_socket_options.cc
@@ -3,6 +3,18 @@
 #include <cstdio>
 #include <limits>
 
+static int getSocketOptRecvBuf(int fd)
+{
+    int opt = 0;
+    socklen_t opt_len = sizeof(opt);
+
+    if (::getsockopt(fd, SOL_SOCKET, SO_RCVBUF, &opt, &opt_len) != 0) {
+        return -1;
+    }
+
+    return opt;
+}
+
 static int getSocketOptSndBuf(int fd)
 {
     int opt = 0;
@@ -15,9 +27,27 @@ static int getSocketOptSndBuf(int fd)
     return opt;
 }
 
+static void setSocketOptRcvBuf(int fd, int opt)
+{
+    ::setsockopt(fd, SOL_SOCKET, SO_RCVBUF, &opt, sizeof(opt));
+}
+
 static void setSocketOptSndBuf(int fd, int opt)
 {
     ::setsockopt(fd, SOL_SOCKET, SO_SNDBUF, &opt, sizeof(opt));
+}
+
+static void printSocketOptRecvBuf(int fd, const char *socket_type)
+{
+    int default_size = getSocketOptRecvBuf(fd);
+    setSocketOptRcvBuf(fd, 0);
+    int min_size = getSocketOptRecvBuf(fd);
+    setSocketOptRcvBuf(fd, std::numeric_limits<int>::max());
+    int max_size = getSocketOptRecvBuf(fd);
+
+    ::printf("%s recv_buffer_size(SO_RCVBUF): "
+             "min: %d default: %d max: %d\n",
+             socket_type, min_size, default_size, max_size);
 }
 
 static void printSocketOptSndBuf(int fd, const char *socket_type)
@@ -33,6 +63,7 @@ static void printSocketOptSndBuf(int fd, const char *socket_type)
              socket_type, min_size, default_size, max_size);
 }
 
+
 static void showTcpOptions()
 {
     int fd = ::socket(AF_INET, SOCK_STREAM, 0);
@@ -40,6 +71,7 @@ static void showTcpOptions()
         return;
     }
 
+    printSocketOptRecvBuf(fd, "tcp_socket");
     printSocketOptSndBuf(fd, "tcp_socket");
 
     ::close(fd);
@@ -52,6 +84,7 @@ static void showUdpOptions()
         return;
     }
 
+    printSocketOptRecvBuf(fd, "udp_socket");
     printSocketOptSndBuf(fd, "udp_socket");
 
     ::close(fd);
