76ab9ceaa2f8b5a67c6381908733bd6a6d8cdc87
kaien <kaienkira@gmail.com>
2019-04-10 10:13:43 +0800

    work sync

---
+++ bin/util/xml_formatter.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+set -o pipefail
+
+script_name=`basename "$0"`
+
+if [ $# -ne 1 ]
+then
+    echo "usage: $script_name <xml_file>"
+    exit 1
+fi
+
+xml_file=$1
+
+sed 's/\r//g' "$xml_file" | awk '
+function print_line(indent, line)
+{
+    for (i = 0; i < indent; ++i) {
+        printf(" ");
+    }
+    printf("%s\r\n", line);
+}
+
+BEGIN{
+    indent = 0;
+    indent2 = 0;
+}
+
+/^(\s*)$/ {
+    printf("\r\n");
+    next;
+}
+
+# <!--
+/^(\s*)<!--.*$/ {
+    printf("%s\r\n", $0);
+    next;
+}
+
+# <tag/>
+match($0, /^(\s*)(<\w+.*\/>.*)$/, m) {
+    print_line(indent * 2, m[2]);
+    indent2 = 0;
+    next;
+}
+
+# <tag>
+match($0, /^(\s*)(<\w+.*>.*)$/, m) {
+    print_line(indent * 2, m[2]);
+    indent += 1;
+    next;
+}
+
+# <tag
+match($0, /^(\s*)(<\w+)(.*)$/, m) {
+    print_line(indent * 2, m[2] m[3]);
+    indent2 = indent * 2 + length(m[2]) + 1;
+    indent += 1;
+    next;
+}
+
+# </tag>
+match($0, /^(\s*)(<\/\w+.*>.*)$/, m) {
+    indent -= 1;
+    print_line(indent * 2, m[2]);
+    next;
+}
+
+# />
+match($0, /^(\s*)([^<]*\/>.*)$/, m) {
+    indent -= 1;
+    print_line(indent2, m[2]);
+    next;
+}
+
+match($0, /^(\s*)(.*)$/, m) {
+    print_line(indent2, m[2]);
+    next;
+}
+
+{
+    printf("%s\r\n", $0);
+}
+'
+if [ $? -ne 0 ]; then exit 1; fi
+
+exit 0
