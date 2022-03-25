d0f1a056250aa6daf962a9c964384e6707348ca9
kaien <kaienkira@gmail.com>
2022-03-25 16:46:48 +0800

    work sync

--- etc/archlinux/install.sh
+++ etc/archlinux/install.sh
@@ -47,26 +47,26 @@ create_dir ~/local/firefox-profile 755
 create_dir ~/local/firefox-profile/profile-1 700
 create_dir ~/local/firefox-profile/profile-2 700
 
-copy_file "$script_path/../_vimrc" ~/.vimrc 600
-copy_file "$script_path/../_gitconfig" ~/.gitconfig 600
-copy_file "$script_path/../_gitignore" ~/.gitignore 600
-copy_file "$script_path/_fonts.conf" ~/.fonts.conf 600
-copy_file "$script_path/_xinitrc" ~/.xinitrc 600
-copy_file "$script_path/_Xdefaults" ~/.Xdefaults 600
-copy_file "$script_path/openbox_rc.xml" ~/.config/openbox/rc.xml 600
-copy_file "$script_path/openbox_menu.xml" ~/.config/openbox/menu.xml 600
-copy_file "$script_path/openbox_autostart" ~/.config/openbox/autostart 600
-copy_file "$script_path/tint2rc" ~/.config/tint2/tint2rc 600
-copy_file "$script_path/i3_config" ~/.config/i3/config 600
-copy_file "$script_path/sway_config" ~/.config/sway/config 600
-copy_file "$script_path/sakura.conf" ~/.config/sakura/sakura.conf 600
-copy_file "$script_path/fcitx5_profile" ~/.config/fcitx5/profile 600
-copy_file "$script_path/fcitx5_classicui.conf" ~/.config/fcitx5/conf/classicui.conf 600
-copy_file "$script_path/systemd/tl-client.service" \
+copy_file "$script_path"/../_vimrc ~/.vimrc 600
+copy_file "$script_path"/../_gitconfig ~/.gitconfig 600
+copy_file "$script_path"/../_gitignore ~/.gitignore 600
+copy_file "$script_path"/_fonts.conf ~/.fonts.conf 600
+copy_file "$script_path"/_xinitrc ~/.xinitrc 600
+copy_file "$script_path"/_xprofile ~/.xprofile 600
+copy_file "$script_path"/openbox_rc.xml ~/.config/openbox/rc.xml 600
+copy_file "$script_path"/openbox_menu.xml ~/.config/openbox/menu.xml 600
+copy_file "$script_path"/openbox_autostart ~/.config/openbox/autostart 600
+copy_file "$script_path"/tint2rc ~/.config/tint2/tint2rc 600
+copy_file "$script_path"/i3_config ~/.config/i3/config 600
+copy_file "$script_path"/sway_config ~/.config/sway/config 600
+copy_file "$script_path"/sakura.conf ~/.config/sakura/sakura.conf 600
+copy_file "$script_path"/fcitx5_profile ~/.config/fcitx5/profile 600
+copy_file "$script_path"/fcitx5_classicui.conf ~/.config/fcitx5/conf/classicui.conf 600
+copy_file "$script_path"/systemd/tl-client.service \
           ~/.config/systemd/user/tl-client.service 600
-copy_file "$script_path/applications/firefox-1.desktop" \
+copy_file "$script_path"/applications/firefox-1.desktop \
           ~/.local/share/applications/firefox-1.desktop 600
-copy_file "$script_path/applications/firefox-2.desktop" \
+copy_file "$script_path"/applications/firefox-2.desktop \
           ~/.local/share/applications/firefox-2.desktop 600
 
 exit 0
