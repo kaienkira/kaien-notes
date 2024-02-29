710931ae7d8c83bb6cbc8e4796740ebad1f04aeb
kaien <kaienkira@gmail.com>
2024-02-29 17:09:17 +0800

    work sync

--- etc/gentoo/init_p3.sh
+++ etc/gentoo/init_p3.sh
@@ -2,32 +2,58 @@
 
 set -o pipefail
 
-echo \
-'sys-kernel/linux-firmware linux-fw-redistributable
-'>/etc/portage/package.license
-
-emerge \
-sys-kernel/linux-firmware \
-sys-process/htop \
-app-editors/vim
-if [ $? -ne 0 ]; then exit 1; fi
-
-install_package_for_intel_cpu()
+install_base_common()
 {
-    echo \
-    'sys-firmware/intel-microcode intel-ucode' \
-    >>/etc/portage/package.license
+    mkdir -p /etc/portage/package.license/
     if [ $? -ne 0 ]; then return 1; fi
 
+    printf '%s\n' \
+    'sys-kernel/linux-firmware linux-fw-redistributable' \
+    >/etc/portage/package.license/base_common
+
     emerge \
-    sys-firmware/intel-microcode
+        sys-kernel/linux-firmware \
+        sys-process/htop \
+        app-editors/vim
     if [ $? -ne 0 ]; then return 1; fi
 
     return 0
 }
 
-# install_package_for_intel_cpu
+install_base_intel()
+{
+    printf '%s\n' \
+    'sys-firmware/intel-microcode intel-ucode' \
+    >/etc/portage/package.license/base_intel
+    if [ $? -ne 0 ]; then return 1; fi
+
+    emerge \
+        sys-firmware/intel-microcode
+    if [ $? -ne 0 ]; then return 1; fi
+
+    return 0
+}
+
+install_kernel()
+{
+    printf '%s\n%s\n%s\n' \
+    'sys-apps/systemd boot' \
+    'sys-kernel/installkernel systemd-boot dracut uki' \
+    >/etc/portage/package.use/kernel
+
+    emerge \
+        sys-kernel/gentoo-kernel
+    if [ $? -ne 0 ]; then return 1; fi
+
+    return 0
+}
+
+install_base_common
+if [ $? -ne 0 ]; then exit 1; fi
+# install_base_intel
 # if [ $? -ne 0 ]; then exit 1; fi
+install_kernel
+if [ $? -ne 0 ]; then exit 1; fi
 
 emerge -c
 if [ $? -ne 0 ]; then exit 1; fi
