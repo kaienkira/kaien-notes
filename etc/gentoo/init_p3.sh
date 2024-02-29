e1bd44107a9d18846cffa2db4e7f8f68c5242e8e
kaien <kaienkira@gmail.com>
2024-02-29 10:18:34 +0800

    work sync

--- etc/gentoo/init_p3.sh
+++ etc/gentoo/init_p3.sh
@@ -9,9 +9,25 @@ echo \
 emerge \
 sys-kernel/linux-firmware \
 sys-process/htop \
-app-editors/vim \
-
+app-editors/vim
 if [ $? -ne 0 ]; then exit 1; fi
+
+install_package_for_intel_cpu()
+{
+    echo \
+    'sys-firmware/intel-microcode intel-ucode' \
+    >>/etc/portage/package.license
+    if [ $? -ne 0 ]; then return 1; fi
+
+    emerge \
+    sys-firmware/intel-microcode
+    if [ $? -ne 0 ]; then return 1; fi
+
+    return 0
+}
+
+# install_package_for_intel_cpu
+# if [ $? -ne 0 ]; then exit 1; fi
 
 emerge -c
 if [ $? -ne 0 ]; then exit 1; fi
