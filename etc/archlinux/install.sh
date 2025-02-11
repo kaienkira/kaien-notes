cb31e745df96c3c9473d8efdcd3424618b348b47
kaien <kaienkira@gmail.com>
2025-02-11 21:42:57 +0800

    work sync

--- etc/archlinux/install.sh
+++ etc/archlinux/install.sh
@@ -30,6 +30,7 @@ create_dir()
 }
 
 create_dir ~/.config 700
+create_dir ~/.config/nvim 700
 create_dir ~/.config/openbox 700
 create_dir ~/.config/tint2 700
 create_dir ~/.config/i3 700
@@ -54,12 +55,13 @@ create_dir ~/local/firefox-profile 755
 create_dir ~/local/firefox-profile/profile-1 700
 create_dir ~/local/firefox-profile/profile-2 700
 
-copy_file "$script_path"/../_vimrc ~/.vimrc 600
-copy_file "$script_path"/../_gitconfig ~/.gitconfig 600
-copy_file "$script_path"/../_gitignore ~/.gitignore 600
+copy_file "$script_path"/_vimrc ~/.vimrc 600
+copy_file "$script_path"/_gitconfig ~/.gitconfig 600
+copy_file "$script_path"/_gitignore ~/.gitignore 600
 copy_file "$script_path"/_fonts.conf ~/.fonts.conf 600
 copy_file "$script_path"/_xinitrc ~/.xinitrc 600
 copy_file "$script_path"/_xprofile ~/.xprofile 600
+copy_file "$script_path"/nvim_init.lua ~/.config/nvim/init.lua 600
 copy_file "$script_path"/openbox_rc.xml ~/.config/openbox/rc.xml 600
 copy_file "$script_path"/openbox_menu.xml ~/.config/openbox/menu.xml 600
 copy_file "$script_path"/openbox_autostart ~/.config/openbox/autostart 600
