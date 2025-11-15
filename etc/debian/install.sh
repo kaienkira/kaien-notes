ef54623edd773add5bc9467d8877aaeab11604b3
kaien <kaienkira@gmail.com>
2025-11-15 14:08:34 +0800

    work sync

---
+++ etc/debian/install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -o pipefail
+
+script_name=`basename $0`
+script_abs_name=`readlink -f "$0"`
+script_path=`dirname "$script_abs_name"`
+
+copy_file()
+{
+    local src_file=$1
+    local dest_file=$2
+    local dest_file_mod=$3
+
+    cp "$src_file" "$dest_file"
+    if [ $? -ne 0 ]; then exit 1; fi
+    chmod "$dest_file_mod" "$dest_file"
+    if [ $? -ne 0 ]; then exit 1; fi
+}
+
+create_dir()
+{
+    local dir_path=$1
+    local dir_mod=$2
+
+    mkdir -p "$dir_path"
+    if [ $? -ne 0 ]; then exit 1; fi
+    chmod "$dir_mod" "$dir_path"
+    if [ $? -ne 0 ]; then exit 1; fi
+}
+
+create_dir ~/.config 700
+create_dir ~/.config/nvim 700
+
+copy_file "$script_path"/../archlinux/_vimrc ~/.vimrc 600
+copy_file "$script_path"/../archlinux/_gitconfig ~/.gitconfig 600
+copy_file "$script_path"/../archlinux/_gitignore ~/.gitignore 600
+copy_file "$script_path"/../archlinux/nvim_init.lua ~/.config/nvim/init.lua 600
+
+exit 0
