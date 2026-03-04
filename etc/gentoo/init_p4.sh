3996ffb0f8853b3ca288d34746a3d8f9134eb5e6
kaien <kaienkira@gmail.com>
2026-03-04 17:57:28 +0800

    work sync

---
+++ etc/gentoo/init_p4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o pipefail
+
+install_system_tools()
+{
+    emerge \
+        app-portage/portage-utils
+    if [ $? -ne 0 ]; then return 1; fi
+
+    return 0
+}
+
+install_system_tools
+if [ $? -ne 0 ]; then exit 1; fi
+
+return 0
