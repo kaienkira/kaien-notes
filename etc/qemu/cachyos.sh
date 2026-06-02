74f5d621aa83a7499cf389374b289a595369892e
kaien <kaienkira@gmail.com>
2026-06-02 17:38:48 +0800

    work sync

--- etc/qemu/cachyos.sh
+++ etc/qemu/cachyos.sh
@@ -6,7 +6,7 @@ qemu-system-x86_64 \
     -hda $HOME/local/vm/cachyos/cachyos.qcow2 \
     -nic user,model=virtio \
     -display egl-headless \
-    -device virtio-vga-gl,xres=1920,yres=1080 \
+    -device virtio-vga-gl,xres=1920,yres=1080,hostmem=8G,blob=true,venus=true \
     -device virtio-serial-pci \
     -object secret,id=sec0,format=raw,file=$HOME/local/vm/tls-key/login_pass \
     -spice port=5901,disable-ticketing=off,password-secret=sec0,x509-dir=$HOME/local/vm/tls-key/ \
